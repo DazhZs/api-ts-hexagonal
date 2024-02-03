6bd76bdbbade410e1815bd08ca8742e4d90c9cad
DazhZs <aleximchv@gmail.com>
2024-02-03 01:03:45 -0600

    configurar db.config.ts

--- app.ts
+++ app.ts
@@ -1,1 +1,19 @@
+import express from 'express';
+import cors from 'cors';
 
+const app = express();
+const PORT = 3000;
+
+app.disable('x-powered-by');
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: 'GET, POST, PUT, DELETE',
+  })
+);
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server runnig on port ${PORT}`);
+});
